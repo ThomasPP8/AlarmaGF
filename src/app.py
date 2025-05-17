2dd6efcdc98939ffa18fb26ca5d24702781baaa5
ThomasPP <thomas.dragon2004@gmail.com>
2025-05-17 10:33:25 -0500

    documentacion en app.py

--- src/app.py
+++ src/app.py
@@ -5,18 +5,18 @@ import os
 from config.mongodb import mongo
 from routes.registros import registros_bp
 
-config = load_dotenv()
+config = load_dotenv() # Cargar variables de entorno desde el archivo .env
 
-app = Flask(__name__)
+app = Flask(__name__) # Configurar la aplicación Flask
 
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-mongo.init_app(app)
+app.config['MONGO_URI'] = os.getenv('MONGO_URI') # Configurar la URI de MongoDB desde las variables de entorno
+mongo.init_app(app) # Inicializar la conexión a MongoDB
 
-@app.route('/')
-def home():
+@app.route('/') # Ruta principal
+def home(): # Renderizar la plantilla index.html
     return render_template('index.html')
 
-app.register_blueprint(registros_bp, url_prefix='/registros')
+app.register_blueprint(registros_bp, url_prefix='/registros') # Registrar el blueprint de registros
 
-if __name__=="__main__":
+if __name__=="__main__": # Ejecutar la aplicación
     app.run(debug=True)
